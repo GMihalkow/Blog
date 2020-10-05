29a6e4d2ee7c8f3576e2c036acf8d167a16a15b8
GMihalkov <georgimihalkov@abv.bg>
2020-10-05 22:20:23 +0300

    feat: added string and enumerable extensions

---
+++ Blog.Web/Infrastructure/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Blog.Web.Infrastructure.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<T> OptimizedSkip<T>(this IEnumerable<T> collection, int skipAmount)
+        {
+            var list = collection.ToList();
+
+            for (int i = skipAmount - 1; i < list.Count; i++)
+            {
+                yield return list[i];
+            }
+        }
+    }
+}
---
+++ Blog.Web/Infrastructure/Extensions/StringExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace Blog.Web.Infrastructure.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool IsNullOrEmpty(this string str)
+            => string.IsNullOrWhiteSpace(str) || str.Length == 0;
+    }
+}
