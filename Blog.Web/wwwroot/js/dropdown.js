8b35a529cfc6f2d96f8070d615cd485696e42686
GMihalkov <georgimihalkov@abv.bg>
2020-10-24 14:41:37 +0300

    feat: added dropdown menus in the menu

---
+++ Blog.Web/wwwroot/js/dropdown.js
@@ -0,0 +1,27 @@
+﻿$(function () {
+    var dropdownBtns = $('#hidden-nav-btn, .dropdown-btn');
+
+    dropdownBtns.on('click', function (e) {
+        var $currentBtn = $(e.currentTarget);
+
+        var dropdown = $currentBtn.next();
+
+        var attrValue = dropdown.attr('style') || '';
+
+        if (attrValue.includes('block')) {
+            dropdown.fadeOut(200);
+        } else {
+            dropdown.fadeIn(500);
+        }
+    });
+
+    window.onclick = function (e) {
+        var $target = $(e.target);
+
+        var targetClasses = $target.attr('class') || '';
+
+        if (!targetClasses.includes('dropdown')) {
+            $('.dropdown').fadeOut(200);
+        }
+    };
+});
