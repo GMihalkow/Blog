d12372b80101d0ad155353128480db695dae3e64
GMihalkov <georgimihalkov@abv.bg>
2020-10-24 14:43:33 +0300

    feat: extracted a user service

---
+++ Blog.Dal/Services/Users/Contracts/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Blog.Web.Services.Users.Contracts
+{
+    public interface IUserService
+    {
+        Task<string> GetUserIdByName(string name);
+    }
+}
---
+++ Blog.Dal/Services/Users/UserService.cs
@@ -0,0 +1,26 @@
+﻿using Blog.Data;
+using Blog.Web.Services.Users.Contracts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+
+namespace Blog.Dal.Services.Users
+{
+    public class UserService : IUserService
+    {
+        private readonly BlogDbContext _dbContext;
+
+        public UserService(BlogDbContext dbContext)
+            => this._dbContext = dbContext;
+
+        public async Task<string> GetUserIdByName(string name)
+        {
+            var user = await this._dbContext.Users.FirstOrDefaultAsync(u => u.UserName == name);
+
+            if (user == null)
+                throw new InvalidOperationException("No user found with this username.");
+
+            return user.Id;
+        }
+    }
+}
