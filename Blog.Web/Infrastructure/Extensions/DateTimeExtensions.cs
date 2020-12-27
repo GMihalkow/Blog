601e4ef5cfa313a9b5f42d349d8e20cddeb1d487
GMihalkov <georgimihalkov@abv.bg>
2020-12-27 13:36:20 +0200

    feat: added DateTimeExtensions

---
+++ Blog.Web/Infrastructure/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Blog.Web.Infrastructure.Constants;
+using System;
+
+namespace Blog.Web.Infrastructure.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static string ToWebDateFormat(this DateTime dateTime) => dateTime.ToString(WebConstants.WebDateFormat);
+    }
+}
