a60d827917daea9ccf667b8272ce73c144fecb5c
GMihalkov <georgimihalkov@abv.bg>
2021-01-17 15:12:07 +0200

    fix: fixed nav background color

---
+++ Blog.Dal/Models/Article/ArticleIndexModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Blog.Dal.Models.Article
+{
+    public class ArticleIndexModel
+    {
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string CoverUrl { get; set; }
+    }
+}
---
+++ Blog.Dal/Models/Index/IndexPageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Blog.Dal.Models.Article;
+using System.Collections.Generic;
+
+namespace Blog.Dal.Models.Index
+{
+    public class IndexPageViewModel
+    {
+        public IEnumerable<ArticleIndexModel> PopularArticles { get; set; }
+
+        public IEnumerable<ArticleIndexModel> LatestArticles { get; set; }
+    }
+}
